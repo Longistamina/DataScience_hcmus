56d46aa2ad12aab18af97a38fadefa1783229f8b
Longistamina <dophamthelong03@outlook.com>
2025-05-09 21:42:21 +0700

    Add files via upload

---
+++ 7.R_Rstudio/Chap_2_DataTypes_Variables_Operators_ImportExport_String_Date/1_Datatypes_logical_numeric_integer_character.R
@@ -0,0 +1,15 @@
+#------- Logical ------#
+pass <- TRUE
+print(class(pass))
+
+#------- Numeric ------#
+a <- 12.3
+print(class(a))
+
+#------- Integer ------#
+b <- 12L
+print(class(b))
+
+#------- Character ------#
+char <- "I Ching"
+print(class(char))
