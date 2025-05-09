39536d5e27d4903a5eddb37f5409181121daaa01
Longistamina <dophamthelong03@outlook.com>
2025-05-09 21:41:55 +0700

    Add files via upload

---
+++ 7.R_Rstudio/Chap_1_Introduction_to_R_Install_Packages/Chap1_Exercise_Sum_Substract_Multiply_Divide.R
@@ -0,0 +1,15 @@
+a <- 3
+b <- 2
+
+sum = a+b
+subtract = a-b
+multiply = a*b
+divide = a/b
+
+print(paste("a + b =", sum))
+
+print(paste("a - b =", subtract))
+
+print(paste("a * b =", multiply))
+
+print(paste("a / b =", divide))
