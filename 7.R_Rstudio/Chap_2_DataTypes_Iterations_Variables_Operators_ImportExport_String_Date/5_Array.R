ec386984e3da3e9e6198797d0325daf6d934de72
Longistamina <dophamthelong03@outlook.com>
2025-05-10 11:06:49 +0700

    Delete 7.R_Rstudio/Chap_2_DataTypes_Iterations_Variables_Operators_ImportExport_String_Date directory

--- 7.R_Rstudio/Chap_2_DataTypes_Iterations_Variables_Operators_ImportExport_String_Date/5_Array.R
+++
@@ -1,6 +0,0 @@
-#Array is a 3D iteration, meaning it contains many 2D matrices
-
-#Create array
-#dim = c(nrows, ncols, nmatrices) (python is nmatrices, nrows, ncols)
-colors <- array(c('red', 'green', 'blue'), dim = c(3,2,2))
-print(colors)
