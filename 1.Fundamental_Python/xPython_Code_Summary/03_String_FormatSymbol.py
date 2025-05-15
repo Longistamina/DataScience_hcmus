3ba6b1098bd36e3a90acf20baf8582d16ecfc63a
Longistamina <dophamthelong03@outlook.com>
2025-05-15 10:01:15 +0700

    Update 03_String_FormatSymbol.py

--- 1.Fundamental_Python/xPython_Code_Summary/03_String_FormatSymbol.py
+++ 1.Fundamental_Python/xPython_Code_Summary/03_String_FormatSymbol.py
@@ -6,10 +6,14 @@ hoten="Trần Ngọc Dung" #character / string
 
 strTT1="Họ tên: "+hoten +"\nTiền lương: "+str(tienluong)
 print(strTT1)
+
 print("-"*50)
+
 strTT2="Họ tên: %s \nTiền lương: %i \nHệ số lương: %f"%(hoten,tienluong,heSoLuong)
 print(strTT2)
+
 print("-"*50)
+
 strTT3="Họ tên: %s \nTiền lương: %i \nHệ số lương: %.2f"%(hoten,tienluong,heSoLuong)
 print(strTT3)
 
