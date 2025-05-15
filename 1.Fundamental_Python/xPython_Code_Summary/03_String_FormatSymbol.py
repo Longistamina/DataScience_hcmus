41157771341ad22b28f3a2409b357c1495523b6a
Longistamina <dophamthelong03@outlook.com>
2025-05-15 10:50:36 +0700

    Update 03_String_FormatSymbol.py

--- 1.Fundamental_Python/xPython_Code_Summary/03_String_FormatSymbol.py
+++ 1.Fundamental_Python/xPython_Code_Summary/03_String_FormatSymbol.py
@@ -1,36 +1,36 @@
-tienluong=5000 #integer (số nguyên)
-heSoLuong=1.475 #float (số thực)
-hoten="Trần Ngọc Dung" #character / string
+salary = 5000 #integer (số nguyên)
+salary_coefficient = 1.475 #float (số thực)
+full_name = "Tran Ngoc Dung" #character / string
 
 #--------------------------------- Format Symbol 1 -----------------------------------------------#
 
-strTT1="Họ tên: "+hoten +"\nTiền lương: "+str(tienluong)
+strTT1="Full ame: "+full_name +"\nSalary: "+str(salary)
 print(strTT1)
 
 print("-"*50)
 
-strTT2="Họ tên: %s \nTiền lương: %i \nHệ số lương: %f"%(hoten,tienluong,heSoLuong)
+strTT2="Full name: %s \nSalary: %i \nSalary coefficient: %f"%(full_name,salary,salary_coefficient)
 print(strTT2)
 
 print("-"*50)
 
-strTT3="Họ tên: %s \nTiền lương: %i \nHệ số lương: %.2f"%(hoten,tienluong,heSoLuong)
+strTT3="Full name: %s \nSalary: %i \nSalary coefficient: %.2f"%(full_name,salary,salary_coefficient)
 print(strTT3)
 
 
 #--------------------------------- Format Symbol 2 -----------------------------------------------#
 
-str1 = f'Họ tên: {hoten}\n Tiền lương: {tienluong}\n Hệ số  lương: {heSoLuong}'
+str1 = f'Full name: {full_name}\n Salary: {salary}\n Salary coefficient: {salary_coefficient}'
 print(str1)
 
 print('-'*50)
 
 #Thêm dấu cách nghìn (dấu phẩy cho tiền lương) và làm tròn heSoLuong 2 chữ số thập phân
-str2 = f'Họ tên: {hoten}\n Tiền lương: {tienluong:,} VND\n Hệ số  lương: {heSoLuong:.2f}'
+str2 = f'Full name: {full_name}\n Salary: {salary:,} VND\n Salary coefficient: {salary_coefficient:.2f}'
 print(str2)
 
 print('-'*50)
 
 #Thêm dấu cách nghìn và làm tròn hai số lẻ cho tiền lương
-str3 = f'Họ tên: {hoten}\n Tiền lương: {tienluong:,.2f} VND\n Hệ số  lương: {heSoLuong}'
+str3 = f'Full name: {full_name}\n Salary: {salary:,.2f} VND\n Salary coefficient: {salary_coefficient}'
 print(str3)
