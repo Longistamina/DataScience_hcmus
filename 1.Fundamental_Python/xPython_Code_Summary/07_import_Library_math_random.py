ede00ba136f4e4c3e9aa6dbaa718d7f61fce0848
Longistamina <dophamthelong03@outlook.com>
2025-05-15 12:01:44 +0700

    Update and rename 07_import_Library_math_random to 07_import_Library_math_random.py

--- 1.Fundamental_Python/xPython_Code_Summary/07_import_Library_math_random.py
+++ 1.Fundamental_Python/xPython_Code_Summary/07_import_Library_math_random.py
@@ -14,3 +14,20 @@ dir(math) #---> list out all the functions, methods and attributes of an object
 decimal, quotient = math.modf(3.5)
 print(decimal)
 print(quotient)
+
+#----------------------------------------------------------#
+#------------------- random Library -----------------------#
+#----------------------------------------------------------#
+import random
+
+n1 = random.random() #Generate a random float number in range of [0,1)
+print(n1)
+
+n2 = random.uniform(10,20) #Generate a random float number x where 10 =< x < 20
+print(n2)
+
+n3 = random.randrange(10,20,1) #Returns a randomly selected element from the specified range (start = 10, stop = 20, step = 1).
+print(n3)
+
+n4 = random.randint(10,20) #Generate a random integer number in range of [10,20]
+print(n4)
