84dcafd64f87544a0733c7a00aca806d223cf3a4
Longistamina <dophamthelong03@outlook.com>
2025-05-15 09:30:04 +0700

    Update 1_print_Command.py

--- 1.Fundamental_Python/xPython_Code_Summary/1_print_Command.py
+++ 1.Fundamental_Python/xPython_Code_Summary/1_print_Command.py
@@ -1,1 +1,11 @@
+print("Hello world!")
+print('Hello world!')
 
+print('''
+Immanuel Kant's Principle of Universalizability:
+
+"Act only according to that maxim whereby you can at the same time will that it should become a universal law."
+
+This principle states that before acting, you should consider whether the rule guiding your action could be applied universally, to everyone, without contradiction. 
+If the action cannot be universalized without causing a contradiction or harm, then it is immoral.
+''')
