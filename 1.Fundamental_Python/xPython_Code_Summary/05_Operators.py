f74c3af5792c32c3f658e7d84b949a12995f9ac9
Longistamina <dophamthelong03@outlook.com>
2025-05-15 10:04:39 +0700

    Create 05_Operators.py

---
+++ 1.Fundamental_Python/xPython_Code_Summary/05_Operators.py
@@ -0,0 +1,37 @@
+#Operators = toán tử
+
+'''
+a+b
+a-b
+a*b
+a**b -> a luỹ thừa b lần (vd: 7**3 = 7*7*7=343)
+a/b -> chia thông thường
+a%b -> chia lấy phần dư
+a//b -> chia lấy phần nguyên
+'''
+
+'''
+a==b so sánh bằng
+a!=b -> a khác b
+>
+<
+>=
+<=
+Kết quả so sánh trả về  True hoặc False
+'''
+
+'''
+a+=b tương đương với a = a+b (cộng dồn số b vào số a)
+a*=b giống với cái trên nhưng là phép nhân
+'''
+
+''' LOGIC
+AND - &: chỉ đúng khi tất cả mệnh đề đều đúng
+OR  - |: chỉ sai khi tất cả mệnh đề đều sai
+NOT - ~: phủ định
+'''
+
+''' IN NOT-IN
+in -> trả về  true nếu giá trị nằm trong tập hợp (vd: 'k'  in 'dek')
+not in -> trả về  true nếu giá trị không nằm trong tập hợp (vd: 'k' not  in 'abc')
+'''
